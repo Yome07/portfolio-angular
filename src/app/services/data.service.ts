5589c1617037fb38c07777e906125f2e07145950
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-15 15:22:03 +0200

    add getters to service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {
+  Contact,
+  Education,
+  Experience,
+  Language,
+  Profile,
+  Project,
+  Skill,
+  SocialLink, TermsOfUse
+} from '../models/portfoilio.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getProfile(): Observable<Profile> {
+    return this.http.get<Profile>(`${this.apiUrl}/profile`);
+  }
+
+  getContact(): Observable<Contact> {
+    return this.http.get<Contact>(`${this.apiUrl}/contact`);
+  }
+
+  getSocialLinks(): Observable<SocialLink> {
+    return this.http.get<SocialLink>(`${this.apiUrl}/social-links`);
+  }
+
+  getSkills(): Observable<Skill> {
+    return this.http.get<Skill>(`${this.apiUrl}/skills`);
+  }
+
+  getLanguages(): Observable<Language> {
+    return this.http.get<Language>(`${this.apiUrl}/languages`);
+  }
+  getExperiences(): Observable<Experience> {
+    return this.http.get<Experience>(`${this.apiUrl}/experiences`);
+  }
+
+  getEducations(): Observable<Education> {
+    return this.http.get<Education>(`${this.apiUrl}/educations`);
+  }
+
+  getProjects(): Observable<Project> {
+    return this.http.get<Project>(`${this.apiUrl}/projects`);
+  }
+
+  getTermsOfUse(): Observable<TermsOfUse> {
+    return this.http.get<TermsOfUse>(`${this.apiUrl}/terms-of-use`);
+  }
+
 }
