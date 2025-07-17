5c0fd5a2afc4792b7da4733231d6fec4ac663508
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-17 16:21:36 +0200

    add hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  downloadCV(): void {
+    // Logique pour télécharger le CV
+    const link = document.createElement('a');
+    link.href = 'assets/cv.pdf'; // Chemin vers votre CV
+    link.download = 'CV_Guillaume_Amselle_Andreux.pdf';
+    link.click();
+  }
 }
