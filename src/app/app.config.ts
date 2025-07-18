a168e68f253af11f29526566cfe0b5b089640f72
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-18 21:39:05 +0200

    add httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { DataService } from './services/data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: DataService,
+      //useFactory: pokemonServiceFactory,
+    },
+  ],
 };
