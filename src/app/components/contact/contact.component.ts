e80fd68ea6bd9f2b0cf5f8a6665cd17f48e4ef87
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-19 11:46:54 +0200

    add component contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-contact',
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  readonly dataService = inject(DataService);
+  readonly social = toSignal(this.dataService.getSocialList());
 
+  getEmailLink(): string {
+    const emailLink = this.social()?.find((link) => link.name === 'email');
+    return emailLink?.url || 'mailto:contact@example.com';
+  }
+
+  getLinkedInLink(): string {
+    const linkedinLink = this.social()?.find(
+      (link) => link.name === 'linkedin'
+    );
+    return linkedinLink?.url || '#';
+  }
 }
