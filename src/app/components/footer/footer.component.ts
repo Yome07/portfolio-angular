16439df57a6bd80c0265b08b84e2736a7e6f64ce
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-19 11:05:23 +0200

    add component footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  readonly dataService = inject(DataService);
+  readonly profile = toSignal(this.dataService.getProfile());
+  readonly termsOfUse = toSignal(this.dataService.getTermsOfUse());
 
+  isTermsModalOpen = false;
+
+  openTermsModal(): void {
+    this.isTermsModalOpen = true;
+  }
+
+  closeTermsModal(): void {
+    this.isTermsModalOpen = false;
+  }
 }
