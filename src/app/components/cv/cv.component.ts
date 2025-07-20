45f6f09bef1d4638704e1921db8893aec4f119b9
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-20 15:02:19 +0200

    add cv component

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { EducationComponent } from '../education/education.component';
 import { ExperienceComponent } from '../experience/experience.component';
 import { SkillsComponent } from '../skills/skills.component';
+
+type CVSection = 'education' | 'experience' | 'skills';
 
 @Component({
   selector: 'app-cv',
@@ -9,4 +11,30 @@ import { SkillsComponent } from '../skills/skills.component';
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss',
 })
-export class CvComponent {}
+export class CvComponent {
+  // section active par défaut
+  activeSection = signal<CVSection>('skills');
+
+  // changer de section
+  selectSection(section: CVSection): void {
+    this.activeSection.set(section);
+  }
+
+  // Vérifier si une section est active
+  isSectionActive(section: CVSection): boolean {
+    return this.activeSection() === section;
+  }
+
+  // Méthode pour télécharger le CV PDF
+  downloadCV(): void {
+    const cvUrl =
+      'assets/CV_developpeur_fullstack_Guillaume_Amselle_Andreux.pdf';
+    const link = document.createElement('a');
+    link.href = cvUrl;
+    link.download = 'CV_Guillaume_Amselle_Andreux.pdf';
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+}
