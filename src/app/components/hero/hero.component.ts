8b1458830da050f19847011106391607a6540b27
Guillaume Amselle Andreux <guillaume@andreux.fr>
2025-07-25 10:53:13 +0200

    delete debug hero component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -9,39 +9,9 @@ import { catchError, of } from 'rxjs';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent {
   readonly dataService = inject(DataService);
-  profile = toSignal(
-    this.dataService.getProfile().pipe(
-      catchError((error: any) => {
-        alert('ERREUR Profile: ' + error.message);
-        return of(null); // Retourne null en cas d'erreur
-      })
-    )
-  );
-
-  constructor() {
-    // Effect pour surveiller les changements du signal
-    effect(() => {
-      const profileData = this.profile();
-      if (profileData) {
-        alert(
-          '2. Données reçues: ' + Object.keys(profileData).length + ' éléments'
-        );
-        alert('3. Signal mis à jour automatiquement');
-      } else if (profileData === null) {
-        alert('4. Erreur détectée dans le signal');
-      }
-    });
-  }
-
-  ngOnInit() {
-    alert('1. Composant chargé');
-    console.log('Profile dans ngOnInit:', this.profile()); // undefined
-
-    // Le signal est undefined ici car la requête HTTP n'est pas encore terminée
-    console.log('Profile dans ngOnInit:', this.profile()); // undefined
-  }
+  profile = toSignal(this.dataService.getProfile());
 
   downloadCV(): void {
     // Logique pour télécharger le CV
